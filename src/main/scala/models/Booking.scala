87d9960c4d077a94a14400906291f92c4f6bf80f
mywyau <47248104+mywyau@users.noreply.github.com>
2024-10-17 23:34:09 +0100

    add some model tests fo bookings

--- src/main/scala/models/Booking.scala
+++ src/main/scala/models/Booking.scala
@@ -17,25 +17,43 @@ case object Cancelled extends BookingStatus
 // Custom Encoder/Decoder for BookingStatus to treat it as a string in JSON
 object BookingStatus {
 
-  def fromString(str: String): BookingStatus = str match {
-    case "Confirmed" => Confirmed
-    case "Pending" => Pending
-    case "Cancelled" => Cancelled
-    case _ => throw new Exception(s"Unknown status: $str")
-  }
+  def fromString(str: String): BookingStatus =
+    str match {
+      case "Confirmed" => Confirmed
+      case "Pending" => Pending
+      case "Cancelled" => Cancelled
+      case _ => throw new Exception(s"Unknown status: $str")
+    }
 
-  implicit val bookingStatusEncoder: Encoder[BookingStatus] = Encoder.encodeString.contramap {
-    case Confirmed => "Confirmed"
-    case Pending => "Pending"
-    case Cancelled => "Cancelled"
-  }
+  implicit val bookingStatusEncoder: Encoder[BookingStatus] =
+    Encoder.encodeString.contramap {
+      case Confirmed => "Confirmed"
+      case Pending => "Pending"
+      case Cancelled => "Cancelled"
+    }
 
-  implicit val bookingStatusDecoder: Decoder[BookingStatus] = Decoder.decodeString.emap {
-    case "Confirmed" => Right(Confirmed)
-    case "Pending" => Right(Pending)
-    case "Cancelled" => Right(Cancelled)
-    case other => Left(s"Invalid booking status: $other")
-  }
+  implicit val bookingStatusDecoder: Decoder[BookingStatus] =
+    Decoder.decodeString.emap {
+      case "Confirmed" => Right(Confirmed)
+      case "Pending" => Right(Pending)
+      case "Cancelled" => Right(Cancelled)
+      case other => Left(s"Invalid booking status: $other")
+    }
+
+  //  implicit val encodeEvent: Encoder[BookingStatus] =
+  //    Encoder.instance {
+  //      case foo@ Confirmed => foo.asJson
+  //      case bar@ Pending => bar.asJson
+  //      case baz@ Cancelled => baz.asJson
+  //    }
+  //
+  //  implicit val decodeEvent: Decoder[BookingStatus] =
+  //    List[Decoder[BookingStatus]](
+  //      Decoder[Confirmed].widen,
+  //      Decoder[Pending].widen,
+  //      Decoder[Cancelled].widen,
+  //    )
+  //      .reduceLeft(_ or _)
 }
 
 case class Booking(
@@ -57,13 +75,3 @@ object Booking {
   implicit val bookingDecoder: Decoder[Booking] = deriveDecoder[Booking]
 }
 
-//http POST http://localhost:8080/api/bookings \
-//  booking_id="booking_6" \
-//  booking_name="Meeting with John" \
-//  user_id:=6 \
-//  workspace_id:=1 \
-//  booking_date="2024-10-20" \
-//  start_time="2024-10-20T09:00:00" \
-//  end_time="2024-10-20T12:00:00" \
-//  status="Confirmed" \
-//  created_at="2024-10-15T17:04:38"
