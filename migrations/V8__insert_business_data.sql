1e8b4d199ab0aa99b4fda34978b398f72d699ce6
mywyau <47248104+mywyau@users.noreply.github.com>
2024-10-20 16:44:11 +0100

    improve business schema, maunally test endpoints

--- migrations/V8__insert_business_data.sql
+++ migrations/V8__insert_business_data.sql
@@ -1,10 +1,9 @@
--- Insert data into the businesses table
-INSERT INTO businesses (user_id, name, contact_email, contact_phone, created_at) VALUES
-    (1, 'Tech Innovations Inc.', 'contact@techinnovations.com', '123-456-7890', '2024-10-01 10:00:00'),
-    (2, 'Health & Wellness Solutions', 'info@wellness.com', '098-765-4321', '2024-10-02 09:30:00'),
-    (3, 'Creative Workspaces', 'hello@creativeworkspaces.com', '111-222-3333', '2024-10-05 14:45:00'),
-    (4, 'Skyline Ventures', 'support@skylineventures.com', '444-555-6666', '2024-10-08 11:15:00'),
-    (5, 'Urban Spaces Co.', 'contact@urbanspaces.com', '777-888-9999', '2024-10-10 16:20:00');
+INSERT INTO business (business_id, business_name, contact_email, contact_phone, created_at) VALUES
+    ('BUS123456', 'Tech Innovations Inc.', 'info@techinnovations.com', '123-456-7890', '2024-10-10 10:30:00'),
+    ('BUS123457', 'Health & Wellness Co.', 'contact@healthwellness.com', '098-765-4321', '2024-10-11 09:45:00'),
+    ('BUS123458', 'Urban Spaces LLC', 'support@urbanspaces.com', '111-222-3333', '2024-10-12 11:15:00'),
+    ('BUS123459', 'Creative Solutions', 'hello@creativesolutions.com', '444-555-6666', '2024-10-13 14:20:00'),
+    ('BUS123460', 'Skyline Ventures', 'sales@skylineventures.com', '777-888-9999', '2024-10-14 16:00:00');
 
 -- Insert data into the facilities table
 INSERT INTO facilities (name) VALUES
